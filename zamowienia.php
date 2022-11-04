90c295bbf4053781582b8dd1eff1ae74d355b18b
GoodWoofie <kufelelo@gmail.com>
2022-11-04 01:19:30 +0100

    logowanie do admin

--- zamowienia.php
+++ zamowienia.php
@@ -1,35 +1,37 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-    var_dump($_REQUEST);
-    var_dump($_SESSION);
-    if(isset($_REQUEST['id'])){
-        if(isset($_SESSION['cart'])) {
+    //var_dump($_REQUEST);
+    //var_dump($_SESSION);
+    if (isset($_REQUEST['id'])) {
+        if (isset($_SESSION['cart'])) {
             array_push($_SESSION['cart'], $_REQUEST['id']);
         } else {
             $_SESSION['cart'] = array();
             array_push($_SESSION['cart'], $_REQUEST['id']);
         }
     }
-    if(isset($_REQUEST['remove']))
-    array_splice($_SESSION['cart'], $_REQUEST['remove'], 1);
+    if (isset($_REQUEST['remove']))
+        array_splice($_SESSION['cart'], $_REQUEST['remove'], 1);
     if (isset($_SESSION['cart'])) {
         $db = new mysqli('localhost', 'root', '', 'ristorante');
         $q = "SELECT nazwa, cena, id FROM dania";
         $result = $db->query($q);
         $names = array();
         $prices = array();
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $id = $row['id'];
             $name = $row['nazwa'];
             $price = $row['cena'];
@@ -38,17 +40,17 @@
         }
         echo '<table>';
         $sum = 0;
-        foreach($_SESSION['cart'] as $id => $cartItem) {
+        foreach ($_SESSION['cart'] as $id => $cartItem) {
             echo '<tr>';
-            echo '<td>'.$names[$cartItem].'</td>';
-            echo '<td>'.$prices[$cartItem]. '</td>';
-            echo '<td><a href="zamowienia.php?remove='.$id.'">Usuń</a></td>';
+            echo '<td>' . $names[$cartItem] . '</td>';
+            echo '<td>' . $prices[$cartItem] . '</td>';
+            echo '<td><a href="zamowienia.php?remove=' . $id . '">Usuń</a></td>';
             echo '</tr>';
             $sum += $prices[$cartItem];
         }
         echo '</table>';
 
-        echo 'Suma zamówienia: '.$sum.'zł';
+        echo 'Suma zamówienia: ' . $sum . 'zł';
     } else {
         echo 'Koszyk jest pusty!';
     }
@@ -66,4 +68,5 @@
         <input type="submit" value="Zamów">
     </form>
 </body>
+
 </html>
